62ebaa8abc28d604f82d423df24977b1a98bf4e9
James Damon <noemail@noemail.com>
2010-06-09 07:30:43 +0000

    Fixed to include registration information and a menu for Sciberquest .

--- development/postproc/crop.scm
+++ development/postproc/crop.scm
@@ -1,4 +1,11 @@
 ;
+;   ____    _ __           ____               __    ____
+;  / __/___(_) /  ___ ____/ __ \__ _____ ___ / /_  /  _/__  ____
+; _\ \/ __/ / _ \/ -_) __/ /_/ / // / -_|_-</ __/ _/ // _ \/ __/
+;/___/\__/_/_.__/\__/_/  \___\_\_,_/\__/___/\__/ /___/_//_/\__(_)
+;
+;Copyright 2008 SciberQuest Inc.
+;
 ; auto-crops crops the inFile stores to outFile
 (define (sciber-crop inFile outFile )
   (let* ((image (car (gimp-file-load RUN-NONINTERACTIVE inFile inFile )))
@@ -7,6 +14,18 @@
     (plug-in-autocrop 0 image drawable)
     (gimp-displays-flush)
     (gimp-file-save RUN-NONINTERACTIVE image drawable outFile outFile )
-    (display (string-append inFile "->" outFile))
   );let
 );define
+
+(script-fu-register "sciber-crop" 
+                    "Crop"
+                    "Auto-Crops an image "
+                    "Jimi Damon <jdamon@gmail.com>"
+                    "Jimi Damon"
+                    "2010-06-08"
+                    ""
+                    SF-STRING "InputFile" "0"
+                    SF-STRING "Output File" "0"
+                    )
+(script-fu-menu-register "sciber-crop" "<Toolbox>/Xtns/Script-Fu/SciberQuest" )
+
