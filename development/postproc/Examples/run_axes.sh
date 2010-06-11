a5b85ca11315565ee61c4e2f3bb8cd3e8e5d9b8b
James Damon <noemail@noemail.com>
2010-06-11 11:08:31 +0000

    Examples that should run correctly

---
+++ development/postproc/Examples/run_axes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# An example that runs the pictures
+# 
+#
+
+axes.sh "/home/jdamon/TestCase1.png" "foo2.png" "2" "10" "0" "10" 5 15 "test xlabel" "test ylabel" "Grid vs. Node size" 12 13 0.23 "San"
---
+++ development/postproc/Examples/runit2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+./axes.sh TestCase1.png  output.png  4 12  2 10 5 5 "X Axis" "YAxis "  "FOOBAR" 0.08 Courier
