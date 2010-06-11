b3fac92c4351e2db96ba4bbfc3619efa1783e5b3
James Damon <noemail@noemail.com>
2010-06-11 08:29:17 +0000

    Cleared up axes

--- development/postproc/axes.sh
+++ development/postproc/axes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# axes INFILE OUTFILE x0 x1 y0 y1 nx ny xLabel yLabel title xOffset yOffset fontSize fontType"
+# axes INFILE OUTFILE x0 x1 y0 y1 nx ny xLabel yLabel title fontSize fontType"
 #
 
 
@@ -14,6 +14,3 @@ fi
 gimp -i -b "(sciber-axes  \"$1\" \"$2\" \"$3\" \"$4\" \"$5\" \"$6\" $7 $8  \"$9\" \"${10}\" \"${11}\" ${12} \"${13}\"  )" -b '(gimp-exit 0)'
 
 
-#gimp -i -b "(sciber-axes  \"$1\" \"$2\" \"$3\" \"$4\" \"$5\" \"$6\" $7 $8  \"$9\" \"${10}\" \"${11}\" 12 13  0.23 \"San\" )" -b '(gimp-exit 0)'
-#gimp -i -b "(sciber-axes  \"$1\" \"$2\" \"$3\" \"$4\" \"$5\" \"$6\" 5 15  \"test xlabel\" \"test ylabel\" \"BRAND NEW TITLE\" 12 13  0.23 \"San\" )" -b '(gimp-exit 0)'
-#gimp -i -b "(sciber-axes  \"/home/jdamon/TestCase1.png\" \"/home/jdamon/blah2.png\" \"2\" \"10\" \"0\" \"10\" 5 15  \"test xlabel\" \"test ylabel\" \"BRAND NEW TITLE\" 12 13  0.23 \"San\" )" -b '(gimp-exit 0)'
