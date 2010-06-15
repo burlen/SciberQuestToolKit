ea05a4e58671e2c3231132fb8701f2482cea360c
James Damon <noemail@noemail.com>
2010-06-15 15:50:33 +0000

    Fixed script

--- development/postproc/annotate.sh
+++ development/postproc/annotate.sh
@@ -6,7 +6,7 @@
 #    Use a default font type for this 
 if [ $#  -lt  6 ] ; then
   echo "Usage: $0 infile outfile text size xcoord ycoord fonttype"
-  exit 1plug_in_script_fu_eval
+  exit 1
 fi
 #infile=$1
 #outfile=$2
@@ -16,5 +16,7 @@ fi
 #xcoord=$5
 #ycoord=$6
 
-gimp --batch-interpreter plug_in_script_fu_eval -i -b "(sciber-annotate \"$1\" \"$2\" \"$3\" $4 $5 $6 )" -b  '(gimp-quit 0)'
+gimp  --batch-interpreter plug_in_script_fu_eval -i -b "(sciber-annotate \"$1\" \"$2\" \"$3\"  $4 $5 $6 \"$7\" )" -b '(gimp-quit 0)' 
+
+
 
