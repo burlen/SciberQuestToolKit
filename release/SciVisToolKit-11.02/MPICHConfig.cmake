4f4bf681993a272e2bfd6b7d38337748de3d060c
Burlen Loring <burlen.loirng@gmail.com>
2011-04-14 21:02:48 +0000

    Added explicit mpi configuration, PV 3.10.0 changed and this is now required.

--- release/SciVisToolKit-11.02/MPICHConfig.cmake
+++ release/SciVisToolKit-11.02/MPICHConfig.cmake
@@ -4,4 +4,12 @@
 # |                                                                           |
 # +---------------------------------------------------------------------------+
 
+include(FindMPI)
+if (NOT MPI_FOUND)
+  message(SEND_ERROR "MPI is required.")
+endif(NOT MPI_FOUND)
+
 add_definitions("-DMPICH_IGNORE_CXX_SEEK")
+
+include_directories(${MPI_INCLUDE_PATH})
+
