cfd16922b19128e92cda04e1a64a0a6bb77eff84
Burlen Loring <burlen.loirng@gmail.com>
2011-10-11 23:09:07 +0000

    clean up xml utils files not needed.

--- release/SciVisToolKit-11.02/script/config/config-pv3-longhorn-icc-ompi.sh
+++ release/SciVisToolKit-11.02/script/config/config-pv3-longhorn-icc-ompi.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 
-    #
 cmake \
     -DCMAKE_C_COMPILER=/opt/apps/intel/11.1/bin/intel64/icc \
     -DCMAKE_CXX_COMPILER=/opt/apps/intel/11.1/bin/intel64/icpc \
@@ -16,40 +15,14 @@ cmake \
     -DMPI_INCLUDE_PATH=/opt/apps/intel11_1/openmpi/1.3.3/include \
     -DMPI_LIBRARY=/opt/apps/intel11_1/openmpi/1.3.3/lib/libmpi.so \
     -DMPI_LINK_FLAGS=-Wl,--export-dynamic \
-    -DPARAVIEW_BUILD_PLUGIN_Array=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_ChartViewFrame=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_ClientChartView=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_ClientGeoView=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_ClientGeoView2D=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_ClientGraphView=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_ClientRecordView=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_ClientRichTextView=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_ClientTableView=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_ClientTreeView=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_CommonToolbar=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_CosmoFilters=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_GraphLayoutFilterPanel=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_Infovis=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_Manta=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_Moments=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_NetDMFReader=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_Prism=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_PointSprite=ON \
-    -DPARAVIEW_BUILD_PLUGIN_SierraPlotTools=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_SLACTools=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_SQLDatabaseGraphSourcePanel=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_SQLDatabaseTableSourcePanel=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_SplitTableFieldPanel=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_StatisticsToolbar=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_SurfaceLIC=ON \
-    -DPARAVIEW_BUILD_PLUGIN_TableToGraphPanel=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_TableToSparseArrayPanel=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_ThresholdTablePanel=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_ClientGraphViewFrame=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_ClientTreeAreaView=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_VisItDatabaseBridge=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_H5PartReader=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_CoProcessingScriptGenerator=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_AnalyzeNIfTIReaderWriter=OFF \
-    -DPARAVIEW_BUILD_PLUGIN_VisTrails=OFF \
+    -DPARAVIEW_DISABLE_VTK_TESTING=ON \
+    -DPARAVIEW_TESTING_WITH_PYTHON=OFF \
+    -DBOOST_ROOT=/home/01237/bloring/ParaView/boost_1_46_1 \
+    -DVTK_USE_BOOST=ON \
+    -DPARAVIEW_ENABLE_PYTHON=ON \
+    -DPARAVIEW_USE_MPI=ON \
+    -DPARAVIEW_BUILD_PLUGIN=EyeDomeLighting \
+    -DPARAVIEW_USE_VISITBRIDGE=ON \
+    -DVISIT_BUILD_READER_CGNS=OFF \
+    -DVISIT_BUILD_READER_Silo=OFF \
     $* 
