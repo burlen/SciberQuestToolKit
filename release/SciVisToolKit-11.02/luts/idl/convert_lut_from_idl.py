976b2b2dce6fbc0f32bf2b16eee99baa54216987
Burlen Loring <burlen.loirng@gmail.com>
2011-11-09 14:39:34 +0000

    Update exported luts to use a ramp in opacity

--- release/SciVisToolKit-11.02/luts/idl/convert_lut_from_idl.py
+++ release/SciVisToolKit-11.02/luts/idl/convert_lut_from_idl.py
@@ -9,15 +9,18 @@ f_names=open('idl_lut_names.txt')
 names=f_names.readlines()
 f_names.close()
 
+f_all=open('all_idl_cmaps.xml','w')
+f_all.write('<doc>')
+
 q=0
 for name in names:
   name=name.split()[0]
+  print name
   lut=[[],[],[]]
   j=0
   while(j<3):
     i=0
     while(i<14):
-      print q
       vals=dat[q].split()
       for val in vals:
         lut[j].append(float(val)/255.0)
@@ -33,11 +36,17 @@ for name in names:
   b=lut[2]
   f_lut=open('%s.xml'%(name),'w')
   f_lut.write('<ColorMap name="%s" space="RGB">\n'%(name))
+  f_all.write('<ColorMap name="%s" space="RGB">\n'%(name))
   i=0
   while(i<n):
     x=x0+i*dx
-    f_lut.write('  <Point x="%f"  o="1" r="%f" g="%f" b="%f"/>\n'%(x,r[i],g[i],b[i]))
+    f_lut.write('  <Point x="%f"  o="%f" r="%f" g="%f" b="%f"/>\n'%(x,x,r[i],g[i],b[i]))
+    f_all.write('  <Point x="%f"  o="%f" r="%f" g="%f" b="%f"/>\n'%(x,x,r[i],g[i],b[i]))
     i+=1
   f_lut.write('</ColorMap>\n')
+  f_all.write('</ColorMap>\n')
   f_lut.close()
+
+f_all.write('</doc>')
+f_all.close()
 
--- release/SciVisToolKit-11.02/luts/mpl/convert_lut_from_mpl.py
+++ release/SciVisToolKit-11.02/luts/mpl/convert_lut_from_mpl.py
@@ -5,42 +5,7 @@ import matplotlib as pl
 from numpy import *
 from scipy.interpolate import *
 
-#def cmap_discretize(cmap, N):
-  #"""Return a discrete colormap from the continuous colormap cmap.
-
-      #cmap: colormap instance, eg. cm.jet.
-      #N: Number of colors.
-
-  #Example
-      #x = resize(arange(100), (5,100))
-      #djet = cmap_discretize(cm.jet, 5)
-      #imshow(x, cmap=djet)
-  #"""
-  #cdict = cmap._segmentdata.copy()
-  ## N colors
-  #colors_i = linspace(0,1.,N)
-  ## N+1 indices
-  #indices = linspace(0,1.,N+1)
-  #for key in ('red','green','blue'):
-    ## Find the N colors
-    #D = array(cdict[key])
-    #I = interpolate.interp1d(D[:,0], D[:,1])
-    #colors = I(colors_i)
-    ## Place these colors at the correct indices.
-    #A = zeros((N+1,3), float)
-    #A[:,0] = indices
-    #A[1:,1] = colors
-    #A[:-1,2] = colors
-    #print A[:,1]
-    ## Create a tuple for the dictionary.
-    ##L = []
-    ##for l in A:
-    ##    L.append(tuple(l))
-    ##cdict[key] = tuple(L)
-  ## Return colormap object.
-  #return #matplotlib.colors.LinearSegmentedColormap('colormap',cdict,1024)
-
-def write_cmap(cmap, N, name):
+def write_cmap(cmap, N, name, f_all):
   cdict = cmap._segmentdata.copy()
   # N colors
   colors_i = linspace(0,1.,N)
@@ -59,7 +24,7 @@ def write_cmap(cmap, N, name):
     j+=1
 
   n=len(lut[0])
-  x0=-1.0
+  x0=0.0
   x1=1.0
   dx=(x1-x0)/(n-1.0)
   r=lut[0]
@@ -67,26 +32,32 @@ def write_cmap(cmap, N, name):
   b=lut[2]
   f_lut=open('%s.xml'%(name),'w')
   f_lut.write('<ColorMap name="%s" space="RGB">\n'%(name))
+  f_all.write('<ColorMap name="%s" space="RGB">\n'%(name))
   i=0
   while(i<n):
     x=x0+i*dx
-    f_lut.write('  <Point x="%f"  o="1" r="%f" g="%f" b="%f"/>\n'%(x,r[i],g[i],b[i]))
+    f_lut.write('  <Point x="%f"  o="%f" r="%f" g="%f" b="%f"/>\n'%(x,x,r[i],g[i],b[i]))
+    f_all.write('  <Point x="%f"  o="%f" r="%f" g="%f" b="%f"/>\n'%(x,x,r[i],g[i],b[i]))
     i+=1
   f_lut.write('</ColorMap>\n')
+  f_all.write('</ColorMap>\n')
   f_lut.close()
   return
 
 f_names=open('mpl_lut_names.txt')
 names=f_names.readlines()
 f_names.close()
+
+f_all=open('all_mpl_cmaps.xml','w')
+f_all.write('<doc>')
+
 for name in names:
   name=name.split()[0]
   print 'name=\"%s\"'%(name)
   m=mpl.get_cmap(name)
-  print m
-  write_cmap(m,256,name)
+  write_cmap(m,256,name,f_all)
 
-
-
+f_all.write('</doc>')
+f_all.close();
 
 
